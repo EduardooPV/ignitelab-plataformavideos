da7ba7ab52490bd838b0dc0f81828a99cc60e789
EduardooPV <eduardoveltroni@hotmail.com>
2022-06-26 14:55:28 -0300

    finish: aula 5

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,31 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonsQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-      id
-      lessonType
-      availableAt
-      title
-      slug
-    }
-  }
-`;
-
-interface GetLessonsQueryResponse {
-  lessons: {
-    id: string;
-    title: string;
-    slug: string;
-    availableAt: string;
-    lessonType: "live" | "class";
-  }[];
-}
-
 export function Sidebar() {
-  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
-  console.log(data);
+  const { data } = useGetLessonsQuery()
 
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
@@ -34,7 +11,7 @@ export function Sidebar() {
       </span>
 
       <div className="flex flex-col gap-8">
-        {data?.lessons.map((lesson, key) => (
+        {data?.lessons.map((lesson) => (
           <Lesson
             key={lesson.id}
             title={lesson.title}
